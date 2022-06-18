392b8ea686982e75992314f07cfcb5edb86acf38
anciety <anciety@pku.edu.cn>
2022-06-18 23:13:42 +0800

    fix target indicator color

--- src/systems/target_indicator.rs
+++ src/systems/target_indicator.rs
@@ -35,25 +35,17 @@ fn show_target_indicator(
 pub fn create_target_indicator(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut color_materials: ResMut<Assets<ColorMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(1.0, 1.0)),
-                color: Color::rgb(0.0, 0.0, 0.8).into(),
-                ..Default::default()
-            },
-            visibility: Visibility {
-                is_visible: false,
-                ..Default::default()
-            },
-            transform: Transform::from_scale(Vec3::splat(0.01)),
-            ..Default::default()
-        })
-        .insert_bundle(MaterialMesh2dBundle {
-            material: color_materials.add(Color::rgb(0.0, 0.0, 0.8).into()),
+        .spawn_bundle(PbrBundle {
+            material: materials.add(StandardMaterial {
+                base_color: Color::rgb(0.0, 0.0, 0.8),
+                unlit: true,
+                ..default()
+            }),
             mesh: meshes.add(circle_mesh()).into(),
+            transform: Transform::from_scale(Vec3::splat(0.01)),
             ..default()
         })
         .insert(TargetIndicator);
